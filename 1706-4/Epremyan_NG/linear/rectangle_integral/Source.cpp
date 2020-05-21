1303f799afe196025e15c58b8a0cc20d2fcc6ca0
Epremyan Norik <epnorik@yandex.ru>
2020-05-21 13:34:31 +0300

    remake linear version

--- 1706-4/Epremyan_NG/linear/rectangle_integral/Source.cpp
+++ 1706-4/Epremyan_NG/linear/rectangle_integral/Source.cpp
@@ -1,77 +1,44 @@
 #include <iostream>
+#include <ctime>
 // function
-double function(const double* val_table, const int depth) {
-	double result=1;
-	for (int i = 1; i <= depth; i++)
-	{
-		result*=val_table[i * 3 - 3];
-	}
-	//std::cout << "Func:" << result << std::endl;
-	return result;
+
+double function(double x, double y)
+{
+	return sin(x + y) * x * x * cos(x - 2 * y);
 }
 
-double recurs_integrator(const int your_depth, const int depth, double* val_table, const double multipy_step ) {
-	double l_amount = 0;
-	for (double i = val_table[your_depth * 3 - 3]; i < val_table[your_depth * 3 - 2]; i += val_table[your_depth * 3 - 1]) {
-		double start;
-		if (your_depth != depth) {
-			start = val_table[your_depth * 3 - 3];
-			val_table[your_depth * 3 - 3] = i;
-			l_amount+=recurs_integrator(your_depth + 1, depth, val_table,multipy_step);
-			if (your_depth != 1) val_table[your_depth * 3 - 3] = start;
-		}
-		else{
-			start = val_table[your_depth * 3- 3];
-			for (double i = start; i < val_table[your_depth * 3 - 2]; i += val_table[your_depth * 3 - 1]) {
-				val_table[your_depth * 3 - 3] = i;
-				l_amount += multipy_step*function(val_table, depth);
-			}
-			val_table[your_depth * 3 - 3 ] = start;
-			break;
-		}
-	}
-	return l_amount;
-};
 
 int main(int argc, char* argv[]) {
 	
+	clock_t start_time;
+	clock_t end_time;
+
 	//test not enough  arguments
-	if (argc==1) {
+	if (argc<5) {
 			std::cout << "NOT ENOUGH ARGUMENTS!" << std::endl;
 			return 0;
 	}
-	else {
-		std::cout <<"Result:" <<argc << std::endl;
-		if (((argc-2) % (atoi(argv[1]))) != 0) {
-			std::cout << "NOT ENOUGH ARGUMENTS!" << std::endl;
+	else if (argc>5) {
+			std::cout << "TOO MUCH ARGUMENTS!" << std::endl;
 			return 0;
 		}
-	}
-
-	const int depth = atoi(argv[1]);
-	//integral amount var, intefral depth
-	double amount = 0;
-	double multipy_step = 1;
+	//check
+	const double step = 0.01;
+	double Sum = 0;
+	const double x_start = atof(argv[1]);
+	const double x_end= atof(argv[2]);
+	const double y_start = atof(argv[3]);
+	const double y_end = atof(argv[4]);
 	
-	//prepare massive and multipy steps
-	double *value_table = new double[depth*3];
-	for (int i = 0; i < argc - 2; i++) {
-		value_table[i] = atof(argv[i + 2]);
-		std::cout << i << ":" << value_table[i] << std::endl;
-	}
-	for (int i = 1; i <= depth; i++) {
-		multipy_step *= value_table[i * 3 - 1];
-		
-	}
-	std::cout << "MulStp:" << multipy_step << std::endl;
-
-	//count integral amount
-	amount = recurs_integrator(1, depth, value_table,multipy_step);
-	std::cout << "Result:" << amount << std::endl;
-	//
-	/*
-	for (int count = 0; count < argc; ++count)
-		std::cout << count << " " << argv[count] << '\n';
-	*/
+	start_time = clock();
+	for (int k = 0; k < 5; k++)
+		Sum = 0;
+		for (double i = x_start; i < x_end; i += step)
+			for (double j = y_start; j < y_end; j += step)
+				Sum += step*step*function(i, j);
+	end_time = clock();
+	std::cout << "Result:" << Sum << std::endl;
+	std::cout << "Time:" << ((double(end_time-start_time))/5*CLOCKS_PER_SEC) << std::endl;
+	
 	return 0;
 }
